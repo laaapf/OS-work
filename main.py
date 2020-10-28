ae1917bd217be44c701ccfd0538feeb527212c9a
laaapf <lucaspfonseca98@gmail.com>
2020-10-28 17:52:23 -0300

    adicionando processsos as filas prioritarias e de usuario

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import string
 arqEntradas = open("entradas.txt")
 arqLinhas = arqEntradas.readlines()
 arqEntradas.close()
-processos = []
+processosPrioritarios = []
+processosUsuario = []
 tempos = []
 
 for tempo in arqLinhas:
@@ -15,13 +16,14 @@ for tempo in arqLinhas:
 tempos.sort
 
 for tempo in range(0,int(tempos[-1])+1):
-    processos.append([])
-print(processos)
+    processosPrioritarios.append([])
+    processosUsuario.append([])
 
 
 for processo in arqLinhas:
     nome = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
-    print(nome)
     novoProcesso = processo.rstrip('\n').split(',')
-    processos[int(novoProcesso[0])].append(Processo(nome,novoProcesso[1],novoProcesso[2],novoProcesso[4],novoProcesso[5],novoProcesso[3]))
-print(processos)
+    if(int(novoProcesso[1]) == 0):
+        processosPrioritarios[int(novoProcesso[0])].append(Processo(nome,novoProcesso[1],novoProcesso[2],novoProcesso[4],novoProcesso[5],novoProcesso[3]))
+    else:
+        processosUsuario[int(novoProcesso[0])].append(Processo(nome,novoProcesso[1],novoProcesso[2],novoProcesso[4],novoProcesso[5],novoProcesso[3]))
