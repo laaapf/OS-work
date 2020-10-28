e80018560612d636ebfa85b34447493df46c33b7
araujoG <araujo_gabriel@id.uff.br>
2020-10-28 16:58:22 -0300

    bloco de memoria e memoria

---
+++ BlocoDeMemoria.py
@@ -0,0 +1,6 @@
+class BlocoDeMemoria :
+    def __init__(self, tamanho, processo, livre):
+        self.tamanho = tamanho
+        self.processo = processo
+        self.livre = livre
+
---
+++ Memoria.py
@@ -0,0 +1,39 @@
+from BlocoDeMemoria import BlocoDeMemoria
+class Memoria:
+    def __init__(self, listaDeBlocos, tamanhoAlocado, tamanhoMaximo):
+        self.listaDeBlocos = listaDeBlocos
+        self.tamanhoAlocado = tamanhoAlocado
+        self.tamanhoMaximo = tamanhoMaximo
+
+    def atualizaAlocacao(self):
+        alocado = 0
+        for i in self.listaDeBlocos:
+            alocado += i.tamanho
+        self.tamanhoAlocado = alocado
+
+    def firstFit(self,processo):
+        if(self.tamanhoAlocado + processo.tamanho <= self.tamanhoMaximo):
+            pos = 0
+            for bloco in self.listaDeBlocos:
+                if(bloco.livre):
+                    if(bloco.tamanho < processo.tamanho):  #aloca o bloco em uma espaço livre gerando um espaço ocupado e outro livre
+                        bloco.tamanho -= processo.tamanho
+                        self.listaDeBlocos.insert(pos,BlocoDeMemoria(processo.tamanho, processo, False))
+                        return True
+                    if(bloco.tamanho == processo.tamanho):   #o bloco encaixa exatamente no espaço livre
+                        bloco.livre = False
+                        bloco.processo = processo
+                        return True
+                pos += 1
+            self.garbageCollector()
+            self.listaDeBlocos.append(BlocoDeMemoria(processo.tamanho, processo, False))
+            return True
+        else:
+            return False
+            
+    def garbageCollector(self):
+        for b in self.listaDeBlocos:
+            if(b.livre):
+                self.listaDeBlocos.remove()
+        self.atualizaAlocacao()
+        
