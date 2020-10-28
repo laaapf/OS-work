920bceac3fbddaa9ef479036dfa3615896439e95
goncazevedo <jhonatan.goncalves@hotmail.com>
2020-10-28 18:55:15 -0300

    atualizando executa

--- Escalonador.py
+++ Escalonador.py
@@ -8,21 +8,21 @@ class Escalonador(object):
     def executa(self, lPerifericos):
         p = self.filaPronto.pop()
         for cpu in cpus:
-            if not p.prioridade and p.tempoProcesso - p.tempoProcessado <=2:
-                if p.recursosDisponiveis(lPerifericos) == "pronto":
+            if not p.prioridade:
+                if p.recursosDisponiveis(lPerifericos) == "semDisco " and p.tempoProcessado == 0:
+                    p.bloqueia()
+                    self.filaSusDisco.append(p)
+                    p = self.filaPronto.pop()
+                elif p.recursosDisponiveis(lPerifericos) == "semImpressora" and p.tempoProcesso - p.tempoProcessado <=2:
+                    p.bloqueia()
+                    self.filaSusImpressora.append(p)
+                    p = self.filaPronto.pop()
+                elif p.recursosDisponiveis(lPerifericos) == "pronto":
                     if cpu.processo == None:
                         cpu.processa(p)
                         p = self.filaPronto.pop()
                     else:
                         continue
-                elif p.recursosDisponiveis(lPerifericos) == "semDisco ":
-                    p.bloqueia()
-                    self.filaSusDisco.append(p)
-                    p = self.filaPronto.pop()
-                elif p.recursosDisponiveis(lPerifericos) == "semImpressora":
-                    p.bloqueia()
-                    self.filaSusImpressora.append(p)
-                    p = self.filaPronto.pop()
             else:
                 if cpu.processo == None:
                         cpu.processa(p)
